96b07b79cfe9f67d5e3e86b7d575f421bdb6b93c
Gabriel R. de Abreu <gabriel.roberto.abreu@gmail.com>
2022-11-06 15:11:17 -0300

    Fix code coverage

---
+++ backend/tests/Shared/eShopCoffe.Core.Tests/Security/JwtSettingsTests.cs
@@ -0,0 +1,56 @@
+﻿using eShopCoffe.Core.Security;
+
+namespace eShopCoffe.Core.Tests.Email
+{
+    public class JwtSettingsTests
+    {
+        private static readonly string ValidSecret = "SECRET";
+        private static readonly string ValidExpires = "1";
+
+        [Fact]
+        public void Constructor_ShouldGetEnvironmentVariables()
+        {
+            // Arrange
+            Environment.SetEnvironmentVariable("JWT_SECRET", ValidSecret);
+            Environment.SetEnvironmentVariable("JWT_EXPIRES", ValidExpires);
+
+            // Act
+            var jwtSettings = new JwtSettings();
+
+            // Assert
+            jwtSettings.Should().NotBeNull();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Constructor_ShouldThrowException_WhenNotValidSecret(string notValidSecret)
+        {
+            // Arrange
+            Environment.SetEnvironmentVariable("JWT_SECRET", notValidSecret);
+            Environment.SetEnvironmentVariable("JWT_EXPIRES", ValidExpires);
+
+            // Act
+            var exception = Assert.Throws<ArgumentNullException>(() => new JwtSettings());
+
+            // Assert
+            exception.Should().NotBeNull();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Constructor_ShouldThrowException_WhenNotValidExpires(string notValidExpires)
+        {
+            // Arrange
+            Environment.SetEnvironmentVariable("JWT_SECRET", ValidSecret);
+            Environment.SetEnvironmentVariable("JWT_EXPIRES", notValidExpires);
+
+            // Act
+            var exception = Assert.Throws<ArgumentNullException>(() => new JwtSettings());
+
+            // Assert
+            exception.Should().NotBeNull();
+        }
+    }
+}
